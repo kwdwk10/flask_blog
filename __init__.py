5b7cc72d2c7fc6fa904ac6b3ead8f8e6b1f739c2
Takahiro Kawada <zodiac_works_te_ht@msn.com>
2022-02-05 15:40:02 +0900

    chapter5-2

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+app  = Flask(__name__)
 app.config.from_object('flask_blog.config')
 
-@app.route('/')
-def show_entries():
-    return "Hello World!"
+from flask_blog.views import entries
